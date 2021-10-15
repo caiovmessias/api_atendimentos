67c67435b756ba73f1e6a673bdf4939af3937608
Caio Messias <caio.messias@gerencianet.com.br>
2021-10-15 23:43:45 +0000

    Desenvolvimento concluído

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = express();
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro);
+    } else {
+        console.log('Conectado com sucesso!');
 
-app.listen(3000, () => {
-    console.log('Server Started in port 3000');
+        Tabelas.init(conexao);
+        const app = customExpress();
+
+        app.listen(3000, () => {
+            console.log('Server Started in port 3000');
+        });
+    }
 });
-
-app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de atendimentos e está fazendo um GET');
-})
